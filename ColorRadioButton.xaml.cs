54bf1104678bb450b8b3242e45dfa012e7c2e2b4
Eternity <caixiexinwork@126.com>
2013-04-18 15:05:30 +0800

    ver1.0
    实现了简单的绘图功能和画笔设置

---
+++ ColorRadioButton.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+
+namespace MyInkPresenter
+{
+    /// <summary>
+    /// 颜色单选按钮
+    /// </summary>
+    public partial class ColorRadioButton : RadioButton
+    {
+        //依赖属性:颜色选择器选中的颜色       
+        public static readonly DependencyProperty CheckedColorProperty;
+        static ColorRadioButton()
+        {
+            //注册依赖属性
+           CheckedColorProperty =  DependencyProperty.Register("CheckedColor", typeof(Color), typeof(ColorRadioButton),new PropertyMetadata(OnCheckedColorChanged));
+        }
+        //依赖属性设置器（使其能在xaml中使用）
+        public Color CheckColor
+        {
+            set { SetValue(CheckedColorProperty, value); }
+            get { return (Color)GetValue(CheckedColorProperty); }
+        }
+        //依赖属性Changed事件
+        private static void OnCheckedColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            //根据选中颜色更改背景色
+            SolidColorBrush lBrush = new SolidColorBrush((Color)e.NewValue);
+            ColorRadioButton colorRadioButton = sender as ColorRadioButton;
+            colorRadioButton.Background = lBrush;
+        }
+        public ColorRadioButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Ink;
+
+namespace MyInkPresenter
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        //单个墨迹笔画
+        private Stroke  currentStroke = null ;
+        //收集墨迹的画板
+        private RectangleGeometry rgBoard = null;
+        private Color currentColor = Colors.Red ;
+        StylusPointCollection myPointCollection = null;
+        public MainPage()
+        {
+            InitializeComponent();
+            InitPresenterComponent();
+        }
+
+        /// <summary>
+        /// 初始化绘图板相关
+        /// </summary>
+        private void InitPresenterComponent()
+        {
+            rgBoard = new RectangleGeometry();
+            rgBoard.Rect = new Rect(0, 0, myInkPresenter.ActualWidth, myInkPresenter.ActualHeight);
+            myInkPresenter.Clip = rgBoard;
+
+        }
+
+        private void myInkPresenter_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //捕获鼠标焦点
+            myInkPresenter.CaptureMouse();
+            myPointCollection = new StylusPointCollection();
+            myPointCollection.Add(e.StylusDevice.GetStylusPoints(myInkPresenter)); 
+            currentStroke = new Stroke(myPointCollection);
+            //设置画笔属性
+            currentStroke.DrawingAttributes.Color = currentColor;
+            currentStroke.DrawingAttributes.Height = sliderThickness.Value;
+            currentStroke.DrawingAttributes.Width = sliderThickness.Value;
+            myInkPresenter.Strokes.Add(currentStroke);
+        }
+
+        private void myInkPresenter_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (currentStroke != null)
+            {
+                currentStroke.StylusPoints.Add(e.StylusDevice.GetStylusPoints(myInkPresenter));
+            }
+
+        }
+
+        private void myInkPresenter_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            
+            currentStroke = null; 
+        }
+
+        private void myInkPresenter_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+            currentStroke = null; 
+        }
+
+
+
+
+        /// <summary>
+        /// 颜色选择单选按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clrRBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            if (strokeDemo != null)
+            {
+                //获取选中颜色
+                ColorRadioButton clrRBtn = (ColorRadioButton)sender;
+                currentColor = clrRBtn.CheckColor;
+                //更改画笔颜色
+                SolidColorBrush lBrush = new SolidColorBrush(currentColor);
+                strokeDemo.Fill = lBrush;
+            }
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            clrRBtn_Checked(this.green, new RoutedEventArgs());
+        }
+
+
+    }
+}
